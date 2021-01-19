62bcba5b250e7e8d2539afa2a02a0f1d0af2ffbf
Ben Harvie <benharvie94@gmail.com>
2021-01-19 15:14:54 +0000

    feat(email): outputting setFailed on mailjet catch error

--- tools/shared/email.js
+++ tools/shared/email.js
@@ -1,4 +1,7 @@
+"use strict";
+
 import { GraphQLClient, gql } from "graphql-request";
+
 const mailjet = require("node-mailjet").connect(
   process.env.MAILJET_API_KEY,
   process.env.MAILJET_API_SECRET
@@ -20,35 +23,40 @@ const getUserEmailVariables = {
   Nickname: process.env.USER_ID,
 };
 
-const userEmail = await graphQLClient
-  .request(getUserEmailQuery, getUserEmailVariables)
-  .then((response) => response.getUserEmail)
-  .catch((error) => {
-    core.setFailed(
-      `GraphQL failed to fetch the users email with ID# ${process.env.USER_ID}: ${error}`
-    );
-  });
+async () => {
+  const userEmail = await graphQLClient
+    .request(getUserEmailQuery, getUserEmailVariables)
+    .then((response) => response.getUserEmail)
+    .catch((error) => {
+      core.setFailed(
+        `GraphQL failed to fetch the users email with ID# ${process.env.USER_ID}: ${error}`
+      );
+    });
 
-await mailjet
-  .post("send", {
-    version: "v3.1",
-  })
-  .request({
-    Messages: [
-      {
-        From: {
-          Email: "info@huntr.dev",
-          Name: "the huntr team",
-        },
-        To: [
-          {
-            Email: userEmail,
-            Name: userEmail,
+  await mailjet
+    .post("send", {
+      version: "v3.1",
+    })
+    .request({
+      Messages: [
+        {
+          From: {
+            Email: "info@huntr.dev",
+            Name: "the huntr team",
           },
-        ],
-        TemplateID: "TODO: template ID",
-        TemplateLanguage: true,
-        Variables: "TODO: required variables",
-      },
-    ],
-  });
+          To: [
+            {
+              Email: userEmail,
+              Name: userEmail,
+            },
+          ],
+          TemplateID: 2227033,
+          TemplateLanguage: true,
+          Variables: "TODO: required variables",
+        },
+      ],
+    })
+    .catch((error) => {
+      core.setFailed(`MailJet failed to send the notification email: ${error}`);
+    });
+};
