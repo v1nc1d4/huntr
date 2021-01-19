c5d52810a65ed42cdfafdcddb2c8968e68c2dca6
Ben Harvie <benharvie94@gmail.com>
2021-01-19 14:53:10 +0000

    feat(email): send email functionality created

---
+++ tools/shared/email.js
@@ -0,0 +1,54 @@
+import { GraphQLClient, gql } from "graphql-request";
+const mailjet = require("node-mailjet").connect(
+  process.env.MAILJET_API_KEY,
+  process.env.MAILJET_API_SECRET
+);
+
+const graphQLClient = new GraphQLClient(process.env.GRAPHQL_API_URL, {
+  headers: {
+    "x-api-key": `${process.env.GRAPHQL_API_KEY}`,
+  },
+});
+
+const getUserEmailQuery = gql(`
+query getUserEmail($Nickname: String!) {
+    getUserEmail(Nickname: $Nickname)
+}
+`);
+
+const variables = {
+  Nickname: process.env.USER_ID,
+};
+
+const userEmail = await graphQLClient
+  .request(getUserEmailQuery, variables)
+  .then((response) => response.getUserEmail)
+  .catch((error) => {
+    core.setFailed(
+      `GraphQL failed to fetch the users email with ID# ${process.env.USER_ID}: ${error}`
+    );
+  });
+
+await mailjet
+  .post("send", {
+    version: "v3.1",
+  })
+  .request({
+    Messages: [
+      {
+        From: {
+          Email: "info@huntr.dev",
+          Name: "the huntr team",
+        },
+        To: [
+          {
+            Email: userEmail,
+            Name: userEmail,
+          },
+        ],
+        TemplateID: "TODO: template ID",
+        TemplateLanguage: true,
+        Variables: "TODO: required variables",
+      },
+    ],
+  });
